9442b15a6d96305ab83df1197b1519f73af567e6
Ananthapadmanabhan M <ananthapadmanabhanr2@gmail.com>
2026-05-19 11:50:18 +0530

    Add projects field to EmployeeSerializer for project associations

--- emsapiproj/apibackendapp/serializers.py
+++ emsapiproj/apibackendapp/serializers.py
@@ -14,11 +14,15 @@ class EmployeeSerializer(serializers.ModelSerializer):
     # 
     Department = DepartmentSerializer(source='DepartmentId', read_only=True) # to get the department details in the employee details, we need to use the source parameter to specify the field which is the foreign key in the employee model and read_only to make it read only field because we don't want to update the department details from the employee details.
 
+    projects = serializers.PrimaryKeyRelatedField(queryset=Projects.objects.all(), many=True, required=False, write_only=True)
+
+    
+
     projects_details = serializers.StringRelatedField(read_only=True, many=True, source='projects') # to get the project details in the employee details, we need to use the source parameter to specify the field which is the many to many field in the employee model and many to true because it is a many to many field and required false because we don't want to make it a required field in the employee details.
     class Meta:
         model = Employee
         # using the Department details obtained, create a nested json field in the dept details
-        fields = ('EmployeeID', 'EmployeeName', 'Designation', 'Contact', 'DateOfJoining', 'DepartmentId', 'IsActive', 'Department', 'projects_details') # we can also use fields = '__all__' to serialize all the fields of the model but it is not a good practice to use it because it will serialize all the fields of the model which may not be required in the API response.
+        fields = ('EmployeeID', 'EmployeeName', 'Designation', 'Contact', 'DateOfJoining', 'DepartmentId', 'IsActive', 'Department', 'projects_details','projects') # we can also use fields = '__all__' to serialize all the fields of the model but it is not a good practice to use it because it will serialize all the fields of the model which may not be required in the API response.
 
 class ProjectsSerializer(serializers.ModelSerializer):
     employee_details = EmployeeSerializer(source='employees', many=True, read_only=True,required=False) # to get the employee details in the project details, we need to use the source parameter to specify the field which is the many to many field in the projects model and read_only to make it read only field because we don't want to update the employee details from the project details.
